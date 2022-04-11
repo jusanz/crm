26f3acb7655a9fb0b7ffa4ae7cb0e0c84a77374c
zoonaka <zoonaka@huwd.jp>
2022-04-11 15:44:40 +0900

    fix: timezone

--- ical_feeds.py
+++ ical_feeds.py
@@ -6,7 +6,7 @@ from django.contrib.sites.models import Site
 from . import models
 
 class AllSchedulesFeed(ICalFeed):
-    timezone = 'UTC'
+    timezone = 'Asia/Tokyo'
 
     def product_id(self):
         current_site = Site.objects.get_current()
